f14a953fbcb40137afa2d0dfcc28e58964bcd571
Carlis Liu <carlisliu@live.com>
2016-11-21 18:04:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+export default function isType(type) {
+    return function(arg) {
+        if (type === 'Array' && Array.isArray) {
+            return Array.isArray;
+        }
+        return Object.prototype.toString.call(arg) === "[object " + type + "]";
+    }
+}
+
+export var isString = isType('String');
+export var isArray = isType('Array');
+export var isFunction = isType('Function');
+export var isObject = isType('Object');
